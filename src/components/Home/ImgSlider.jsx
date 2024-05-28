6556f397df4af935d13097ebfd233e2ff25e747a
Markjonel02 <markjonel@neksjob.com>
2024-05-28 15:18:29 +0800

    modified:   src/components/Home/ImgSlider.jsx 	modified:   src/components/Services/Servicecards.jsx

--- src/components/Home/ImgSlider.jsx
+++ src/components/Home/ImgSlider.jsx
@@ -29,21 +29,81 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 const Img = [
-  { src: jspng, text: "JavaScript", desc: "" },
-  { src: bootstrap, text: "Bootstrap", desc: "" },
-  { src: mysql, text: "MySQL", desc: "" },
-  { src: postgre, text: "Postgre", desc: "" },
-  { src: physics, text: "React Js", desc: "" },
-  { src: php, text: "PHP", desc: "" },
-  { src: csharp, text: "C#", desc: "" },
-  { src: python, text: "Python", desc: "" },
-  { src: Django, text: "Django", desc: "" },
-  { src: Asp, text: "Asp", desc: "" },
-  { src: Css, text: "Css", desc: "" },
-  { src: Html, text: "Html", desc: "" },
-  { src: jquery, text: "JQuery", desc: "" },
-  { src: Tailwind, text: "Tailwind", desc: "" },
-  { src: Vuejs, text: "Vuejs", desc: "" },
+  {
+    src: jspng,
+    text: "JavaScript",
+    desc: "JavaScript is a versatile language for creating interactive web pages, supporting both client and server-side.",
+  },
+  {
+    src: bootstrap,
+    text: "Bootstrap",
+    desc: "Bootstrap is a popular framework for building responsive, mobile-first websites with pre-designed components and utilities.",
+  },
+  {
+    src: mysql,
+    text: "MySQL",
+    desc: "MySQL is a widely-used relational database management system known for its reliability, scalability, and SQL support.",
+  },
+  {
+    src: postgre,
+    text: "Postgre",
+    desc: "PostgreSQL is a powerful, open-source relational database system known for its extensibility, reliability, and SQL compliance.",
+  },
+  {
+    src: physics,
+    text: "React Js",
+    desc: "React.js is a JavaScript library for building user interfaces, emphasizing component-based development and declarative programming.",
+  },
+  {
+    src: php,
+    text: "PHP",
+    desc: "PHP is a popular server-side scripting language for web development, known for its ease of use and versatility.",
+  },
+  {
+    src: csharp,
+    text: "C#",
+    desc: "C# is a versatile programming language primarily used for developing Windows applications, web services, and games.",
+  },
+  {
+    src: python,
+    text: "Python",
+    desc: "Python is a versatile programming language known for its readability, simplicity, and extensive standard libraries.",
+  },
+  {
+    src: Django,
+    text: "Django",
+    desc: "Python in Django is a framework that enables rapid web development with a robust ORM, URL routing, and templating system.",
+  },
+  {
+    src: Asp,
+    text: "Asp",
+    desc: "ASP (Active Server Pages) is a server-side scripting framework for building dynamic web applications using VBScript or JavaScript.",
+  },
+  {
+    src: Css,
+    text: "Css",
+    desc: "CSS (Cascading Style Sheets) is a language for styling web pages, controlling layout, typography, and visual aspects.",
+  },
+  {
+    src: Html,
+    text: "Html",
+    desc: "HTML (Hypertext Markup Language) is the standard markup language for creating web pages and web applications.",
+  },
+  {
+    src: jquery,
+    text: "JQuery",
+    desc: "jQuery is a fast, small, and feature-rich JavaScript library that simplifies HTML document traversing and manipulation.",
+  },
+  {
+    src: Tailwind,
+    text: "Tailwind",
+    desc: "Tailwind CSS is a utility-first CSS framework that helps create custom designs with minimal, intuitive CSS code.",
+  },
+  {
+    src: Vuejs,
+    text: "Vuejs",
+    desc: "Vue.js is a progressive JavaScript framework for building user interfaces, known for its simplicity and flexibility.",
+  },
 ];
 
 Img.propTypes = {
@@ -127,11 +187,7 @@ const ImgSlider = () => {
                 <Typography variant="h5" color="blue-gray" className="mb-2">
                   {item.text}
                 </Typography>
-                <Typography>
-                  The place is close to Barceloneta Beach and bus stop just 2
-                  min by walk and near to &quot;Naviglio&quot; where you can
-                  enjoy the main night life in Barcelona.
-                </Typography>
+                <Typography>{item.desc}</Typography>
               </CardBody>
               <CardFooter className="pt-0">
                 <Button>Read More</Button>
--- src/components/Services/Servicecards.jsx
+++ src/components/Services/Servicecards.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+import Blob from "../../assets/images/Blob2.svg";
 import {
   Card,
   CardBody,
@@ -5,74 +7,207 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import PropTypes from "prop-types";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Servicecards = () => {
-  const banner = [
+  const Banner = [
     {
-      title: "School Programming Activities",
-      desc: "School programming activities, like coding clubs, hackathons, and competitions, teach coding skills, teamwork, and problem-solving for future tech careers.",
+      title: "Web development",
+      desc: "We create fast, responsive, user-friendly websites, using the latest technologies for optimal search engine performance.",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-10 mb-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+          />
+        </svg>
+      ),
     },
     {
-      title: "Web development Activities",
-      desc: "",
+      title: "School Activities",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-10 mb-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"
+          />
+        </svg>
+      ),
+      desc: "Our activities engage students using diverse methods, catering to various learning styles for dynamic experiences.",
+    },
+    {
+      title: "Consultation",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-10 mb-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+          />
+        </svg>
+      ),
+      desc: "Get personalized advice, expert insights, and tailored solutions. Schedule a session for professional guidance today.",
+    },
+    {
+      title: "Capstone Projects",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-10 mb-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+          />
+        </svg>
+      ),
+      desc: "Our capstone project offers hands-on experience, applying learned skills to real-world challenges and solutions.",
     },
   ];
+
+  useEffect(() => {
+    gsap.fromTo(
+      ".card",
+      { opacity: 0, x: 100 },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: ".card",
+          start: "top 80%",
+          end: "bottom 20%",
+          scrub: true,
+          stagger: 0.1,
+        },
+        duration: 1,
+        ease: "power1.out",
+      }
+    );
+
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      card.addEventListener("mouseenter", () => {
+        gsap.to(card, {
+          x: -10,
+          y: -10,
+          duration: 0.5,
+          ease: "power1.out",
+        });
+      });
+      card.addEventListener("mouseleave", () => {
+        gsap.to(card, {
+          x: 0,
+          y: 0,
+          duration: 0.5,
+          ease: "power1.out",
+        });
+      });
+    });
+
+    return () => {
+      cards.forEach((card) => {
+        card.removeEventListener("mouseenter", () => {});
+        card.removeEventListener("mouseleave", () => {});
+      });
+    };
+  }, []);
+
   return (
     <>
-      <div className="relative">
-        {" "}
-        <Card className="mt-6 w-72">
-          <CardBody>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="mb-4 h-12 w-12 text-gray-900"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-                clipRule="evenodd"
-              />
-              <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-            </svg>
-            <Typography variant="h5" color="blue-gray" className="mb-2">
-              UI/UX Review Check
-            </Typography>
-            <Typography>
-              Because it&apos;s about motivating the doers. Because I&apos;m
-              here to follow my dreams and inspire others.
-            </Typography>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <a href="#" className="inline-block">
-              <Button
-                size="sm"
-                variant="text"
-                className="flex items-center gap-2"
-              >
-                Learn More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="h-4 w-4"
+      <h1
+        className="text-3xl xl:text-5xl md:text-4xl font-Lato-Black text-center text-blue-primary uppercase mt-20"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
+        Services we Offer
+      </h1>
+      <div
+        className="relative flex flex-wrap justify-center items-center gap-6 md:gap-10 p-5 mb-20"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
+        <img
+          src={Blob}
+          alt="Blob"
+          className="absolute top-0 left-0 opacity-50 "
+        />
+        {Banner.map((item, index) => (
+          <Card className="card mt-6 w-full sm:w-72" key={index}>
+            <CardBody>
+              {item.img}
+              <Typography variant="h5" color="blue-gray" className="mb-2">
+                {item.title}
+              </Typography>
+              <Typography>{item.desc}</Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <a href="#" className="inline-block">
+                <Button
+                  size="sm"
+                  variant="text"
+                  className="flex items-center gap-2"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                  />
-                </svg>
-              </Button>
-            </a>
-          </CardFooter>
-        </Card>
+                  Learn More
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                    />
+                  </svg>
+                </Button>
+              </a>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </>
   );
 };
 
 export default Servicecards;
+
+Servicecards.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  img: PropTypes.string,
+};
