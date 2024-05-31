d2a26da33b628c9c35985e895b6d3eb692e033b4
Markjonel02 <markjonel@neksjob.com>
2024-05-31 11:34:26 +0800

    news

--- src/components/Api/Apiservice.jsx
+++ src/components/Api/Apiservice.jsx
@@ -1,44 +1,30 @@
-/* import { useEffect, useState } from "react";
-
-const Apiservice = () => {
-  const [Post, setPost] = useState([]);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPost(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []); 
-  return (
-    <div>
-      {Post.map((post) => (
-        <div key={post.id}>
-          <h1 className="text-3xl text-red-500">{post.title}</h1>
-          <p>{post.body}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Apiservice;
- */
-
 import axios from "axios";
+
+// Ensure the environment variable is correctly accessed
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const getArticles = async () => {
   try {
-    const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=tesla&from=2024-04-30&sortBy=publishedAt&apiKey=${API_KEY}`
-    );
+    const response = await axios.get(`https://newsapi.org/v2/everything`, {
+      params: {
+        q: "apple",
+        from: "2024-05-30",
+        to: "2024-05-30",
+        sortBy: "popularity",
+        apiKey: API_KEY,
+      },
+    });
     console.log(response.data);
     return response.data.articles;
   } catch (error) {
-    console.log("Error fetching top headlines:", error);
+    // Improved error handling
+    if (error.response) {
+      console.error("Error response:", error.response.data);
+    } else if (error.request) {
+      console.error("Error request:", error.request);
+    } else {
+      console.error("Error message:", error.message);
+    }
+    console.error("Error config:", error.config);
   }
 };
